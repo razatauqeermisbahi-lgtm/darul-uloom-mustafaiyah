29df080d5f696033ec84808221cb9d7a8de48a4c
Mohammad Tauqeer <razatauqeermisbahi@gmail.com>
2026-09-08 22:02:33 +0530

    Fix deployment errors

--- src/Pages/CampusPic/Campuspic.jsx
+++ src/Pages/CampusPic/Campuspic.jsx
@@ -11,10 +11,10 @@ import pic2 from "../../assets/gallery-pic2.png";
 import pic3 from "../../assets/gallery-pic3.png";
 import pic4 from "../../assets/gallery-pic4.png";
 import pic5 from "../../assets/dome-pic.png";
-import pic6 from "../../assets/facility01.png";
-import pic7 from "../../assets/facility02.png";
-import pic8 from "../../assets/facility03.png";
-import pic9 from "../../assets/facility04.png";
+import pic6 from "../../assets/Facility01.png";
+import pic7 from "../../assets/Facility02.png";
+import pic8 from "../../assets/Facility03.png";
+import pic9 from "../../assets/Facility04.png";
 
 const galleryImages = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9];
 
